6667b7955d9c6df76fef6ea3413a3c6881ec0076
Andrew Gossage <gossachfront@gmail.com>
2025-03-02 08:56:58 -0700

    config

--- init.lua
+++ init.lua
@@ -246,7 +246,7 @@ require("lazy").setup({
 			-- Useful for getting pretty icons, but requires special font.
 			--  If you already have a Nerd Font, or terminal set up with fallback fonts
 			--  you can enable this
-			-- { 'nvim-tree/nvim-web-devicons' }
+			{ "nvim-tree/nvim-web-devicons" },
 		},
 		config = function()
 			-- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -480,7 +480,6 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
 				--
 
 				lua_ls = {
